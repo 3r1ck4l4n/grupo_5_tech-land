2e6dfe88035c7f86010148881bcf7c655edd54f2
Erick Luna <eluna1802@alumno.ipn.mx>
2021-08-19 13:37:58 -0500

    Modularization of styles for the navbar and the footer.
    The styles were divided into different sheets of the footer and the navigation bar to be implemented independently.

--- app.js
+++ app.js
@@ -7,28 +7,26 @@ const path = require("path");
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
-    console.log("Server running at port 3000");
+app.listen(3000, () => {
+  console.log("Server running at port 3000");
 });
 
-app.get("/productDetail", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
 });
 
-app.get("/home", (req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/home.html"));
-}); 
-
-app.get("/register", (req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/register.html"));
-}); 
-
-app.get("/login", (req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/login.html"));
+app.get("/home", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
 
-app.get("/productCart", (req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/productCart.html"));
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
 
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
+});
