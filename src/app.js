ba393eba8e5777adce664cee417e1af2402d303d
Erick Luna <eluna1802@alumno.ipn.mx>
2021-09-01 23:20:15 -0500

    Preview for image in edit product form has been implemented.

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.static(publicPath));
 
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'./views'));
+app.set('views', path.join(__dirname,'./views/'));
 app.listen(3000, () => {
   console.log("Server running at port 3000");
 });
