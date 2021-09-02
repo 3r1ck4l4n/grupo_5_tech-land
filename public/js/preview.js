ef1a1d1d42f5782fd2e99d72f9253049ce933996
Erick Luna <eluna1802@alumno.ipn.mx>
2021-09-02 12:16:09 -0500

    Edit form has been modificated.

--- public/js/preview.js
+++ public/js/preview.js
@@ -4,11 +4,14 @@ document.getElementById("file").onchange = function(e) {
     reader.onload = function() {
       let preview = document.getElementById('preview'),
         image = document.createElement('img');
-  
-      image.src = reader.result;
-        
+        p= document.createElement("div");
+        image.src = reader.result;
+        p.innerHTML="Vista Previa";
       preview.innerHTML='';
       preview.append(image);
+      preview.append(p);
+      preview.style.boxShadow="0px 0px 10px black";
+      p.style.margin="15px auto auto auto";
     };
   
     reader.readAsDataURL(e.target.files[0]);
